0f294f6dbac62f24aedf58a0ebb5fadab510db6a
Kirti Anand <kirti.a@cisinlabs.com>
2017-12-27 22:37:28 +0530

    Removes pipe from guideline module

--- src/app/guideline/guideline.module.ts
+++ src/app/guideline/guideline.module.ts
@@ -4,14 +4,13 @@ import { MaterialModule } from '../core-ui/material/material.module';
 
 import { GuidelineComponent } from './guideline.component';
 import { GuidelineIconsComponent } from './tabs/guideline-icons/guideline-icons.component';
-import { EscapeHtmlPipePipe } from './escape-html-pipe.pipe';
 
 @NgModule({
   imports: [
     CommonModule,
     MaterialModule
   ],
-  declarations: [GuidelineComponent, GuidelineIconsComponent, EscapeHtmlPipePipe ],
+  declarations: [GuidelineComponent, GuidelineIconsComponent],
   exports: [ CommonModule ]
 })
 export class GuidelineModule { }
