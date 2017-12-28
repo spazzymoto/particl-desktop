5c59a30f78d8605479c4f8ad2e6c09d736ffbfc8
Kirti Anand <kirti.a@cisinlabs.com>
2017-12-28 18:12:26 +0530

    Adds guideline modals component in module

--- src/app/guideline/guideline.module.ts
+++ src/app/guideline/guideline.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../core-ui/material/material.module';
 
+import { SharedModule } from '../wallet/shared/shared.module';
+import { WalletModule } from '../wallet/wallet/wallet.module';
+
 import { GuidelineComponent } from './guideline.component';
 import { GuidelineIconsComponent } from './tabs/guideline-icons/guideline-icons.component';
+import { GuidelineModalsComponent } from './tabs/guideline-modals/guideline-modals.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    MaterialModule
+    MaterialModule,
+    SharedModule,
+    WalletModule
   ],
-  declarations: [GuidelineComponent, GuidelineIconsComponent],
+  declarations: [GuidelineComponent, GuidelineIconsComponent, GuidelineModalsComponent],
   exports: [ CommonModule ]
 })
 export class GuidelineModule { }
